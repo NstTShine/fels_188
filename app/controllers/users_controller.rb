3c12b504b8004f228c7dcd06d40034d13e195335
TShine <tuan.tta1991@gmail.com>
2016-09-28 15:27:41 +0700

    Edit Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new]
+  before_action :authorize_user, only: [:edit, :update]
+  before_action :load_user, only: [:show, :edit, :update]
 
   def index
     @users = User.order(created_at: :desc).paginate page: params[:page],
@@ -6,11 +9,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-       flash[:danger] = t "user_not_found"
-       redirect_to root_path
-    end
   end
 
   def new
@@ -28,9 +26,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :phone_number, :password,
       :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t "user_not_found"
+      redirect_to root_path
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "login_require"
+      redirect_to login_url
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length:{minimum: 6}
+
+  def current_user? user
+    self == user
+  end
 end
